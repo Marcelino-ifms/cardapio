d2dbaec4d66bfad858e4b24658f46ebc551fd541
Marcelino-ifms <140651566+Marcelino-ifms@users.noreply.github.com>
2024-06-11 13:05:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function App() {
+
+    const [listaProdutos, setProdutos] = useState([
+        {
+            id: 1,
+            item: "Hambúrguer",
+            imagem: "https://www.assai.com.br/sites/default/files/shutterstock_1806472312.jpg",
+            preco: "R$ 25,99"
+        },
+        {
+            id: 2,
+            item: "Coca-cola 350ml",
+            imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm4S15squn95k7qtrVOpMX1MOJGe48y4B7FQ&s",
+            preco: "R$ 5,99"
+        },
+        {
+            id: 3,
+            item: "Batatas fritas",
+            imagem: "https://gastronomiacarioca.zonasul.com.br/wp-content/uploads/2023/05/batata_frita_destaque_ilustrativo_zona_sul.jpg",
+            preco: "R$ 8,99"
+        },
+        {
+            id: 4,
+            item: "Suco de Frutas",
+            imagem: "https://helenalunardelli.com.br/wp-content/uploads/2013/02/sucos.jpg",
+            preco: "R$ 8,99"
+        },
+    ]);
+
+
+    
+
+    return (
+        <div className="bloco-principal">
+            <div className="bloco-produtos">
+              
+            </div>
+            <div className="bloco-pedidos">
+                
+            </div>
+        </div>
+    );
+}
